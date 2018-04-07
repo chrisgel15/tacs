0fb10120b733a74b31cf238c2b352f97ff5ab7f3
Christian Gelman <christiangelman@gmail.com>
2018-04-07 18:46:14 -0300

    Added Signup and Signin skeletons

---
+++ Tacs/Controllers/SignupController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tacs.Models;
+
+namespace Tacs.Controllers
+{
+    public class SignupController : ApiController
+    {
+        // POST api/signup
+        public void Post([FromBody]User user)
+        {
+
+        }
+    }
+}
