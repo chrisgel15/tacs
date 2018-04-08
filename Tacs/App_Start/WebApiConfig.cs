4f40d19669677746e41495df276c27e8a9487776
federicomandri85 <fmandri@est.frba.utn.edu.ar>
2018-04-08 09:57:17 -0300

    Se crea nueva ruta para mapear controles - archivo webapiconfig.cs

--- Tacs/App_Start/WebApiConfig.cs
+++ Tacs/App_Start/WebApiConfig.cs
@@ -15,10 +15,18 @@ namespace Tacs
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+                name: "WalletApi",
+                routeTemplate: "api/portfolio/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            
         }
     }
 }
