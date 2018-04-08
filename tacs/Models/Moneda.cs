bc6ac9e5a4facc231756125acbb7d1db6383f9e3
federicomandri85 <fmandri@est.frba.utn.edu.ar>
2018-04-08 17:52:41 -0300

    UserController -> se prueba usar httpClient para pegar api externa. TODO: resolver json.

---
+++ tacs/Models/Moneda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Tacs.Models
+{
+    [DataContract]
+    public class Moneda
+    {
+        [DataMember]
+        public string nombre { get; set; }
+
+        [DataMember]
+        public string id { get; set; }
+
+        [DataMember]
+        public string cotizacion { get; set; }
+    }
+}
