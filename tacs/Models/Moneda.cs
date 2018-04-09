f0529e81b55e10d702bf30c43e3584321f3dea53
federicomandri85 <fmandri@est.frba.utn.edu.ar>
2018-04-08 21:17:53 -0300

    objeto moneda con los campos que devuelve api externa

--- tacs/Models/Moneda.cs
+++ tacs/Models/Moneda.cs
@@ -10,12 +10,34 @@ namespace Tacs.Models
     public class Moneda
     {
         [DataMember]
-        public string nombre { get; set; }
-
-        [DataMember]
         public string id { get; set; }
 
         [DataMember]
-        public string cotizacion { get; set; }
+        public string nombre { get; set; }
+        [DataMember]
+        public string symbol { get; set; }
+        [DataMember]
+        public string rank { get; set; }
+        [DataMember]
+        public string price_usd { get; set; }
+        [DataMember]
+        public string price_btc { get; set; }
+        [DataMember]
+        public string _24h_volume_usd { get; set; }
+        [DataMember]
+        public string market_cap_usd { get; set; }
+        [DataMember]
+        public string available_supply { get; set; }
+        [DataMember]
+        public string percent_change_1h { get; set; }
+        [DataMember]
+        public string percent_change_24h { get; set; }
+        [DataMember]
+        public string perc_ent_change_7d { get; set; }
+        [DataMember]
+        public string last_updated { get; set; }
+
+
+
     }
 }
