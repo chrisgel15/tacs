1ca385bdd603f0110d21ce28df1fcdbbce447932
EricQuinterX <eric.brian.quinteros@gmail.com>
2018-06-27 04:00:48 -0300

    algunos fix del frontend

--- Angular/src/app/config.ts
+++ Angular/src/app/config.ts
@@ -1,4 +1,10 @@
+// Descomentar para consultar la nube
 //export const apiTacs = 'https://tacscripto.azurewebsites.net/api';
+
+// Descomentar para consumir la API local
 export const apiTacs = 'http://localhost:51882/api';
+
+// Descomentar para pasar a produccion
+// export const apiTacs = '/api';
 
 export const apiCoinMarket = 'https://api.coinmarketcap.com/v1';
